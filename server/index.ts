1a27c440b598919b02e4453efdceac0fbaab5dd7
gloe2019 <gloedem@gmail.com>
2021-11-30 20:33:14 -0500

    install mongoose, initialize server

---
+++ server/index.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import express  from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+dotenv.config()
+const PORT = Number(process.env.PORT)
+const app = express()
+app.use(cors());
+app.use(morgan("dev"))
+app.use(express.urlencoded({ extended: true })); //parse URL-encoded bodies
+app.use(express.json()); //parse JSON bodies
+
+//connect to MongoDB
+const dbUrl = process.env["MONGODB_URI"]
+mongoose.connect(dbUrl).then((res) => {
+  console.log(`Connected to Mongo Db:`, process.env.DB_NAME)
+})
+
+app.listen(PORT, () => {
+  console.log(`BuyIt Server running on port: ${PORT}`);
+});
