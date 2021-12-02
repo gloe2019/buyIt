c96050a14639cf93638adecc34083177a296d4a1
gloe2019 <gloedem@gmail.com>
2021-12-02 13:16:11 -0500

    update usermodel to pull interface from IUser

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,27 +1,22 @@
 import mongoose from "mongoose";
-
+import IUser from "../interfaces/user";
 const Schema = mongoose.Schema;
-// 1. Create an interface representing a document in MongoDB.
-interface User {
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-  isAdmin: boolean;
-  // createDate: Date
-  // updateDate: Date
-}
+// 1. Create an interface representing a document in MongoDB --> imported from IUser
 
 // 2. Create a Schema corresponding to the document interface.
-const UserSchema: mongoose.Schema = new Schema<User>(
+const UserSchema: mongoose.Schema = new Schema<IUser>(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    email: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      index: { unique: true },
+    },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, required: true },
   },
   { timestamps: true }
 );
 
-export default mongoose.model<User>("User", UserSchema);
+export default mongoose.model<IUser>("User", UserSchema);
