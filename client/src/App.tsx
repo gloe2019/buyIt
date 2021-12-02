f4fc970aa5f0e7488102202d41195957c1e4e760
gloe2019 <gloedem@gmail.com>
2021-12-02 16:10:58 -0500

    setup routing for components, cleanup..

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, } from "react-router-dom"
+import { Routes } from "react-router-dom"
+import routes from "./config/routes"
+// Routing should be done from here --> connect to backend!
 
-function App() {
+const App: React.FunctionComponent<{}> = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <BrowserRouter>
+        <Routes>
+          {routes.map((route, index) => {
+            
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={<route.component name={route.name}/>}
+              />
+            )
+          })}
+          {/* <Route path="/" element={<Home name="Home"/>} />
+          <Route path="/about" element={<About name="About"/>} /> */}
+      </Routes>
+      </BrowserRouter>
+      </div>
+  )
 }
 
-export default App;
+export default App
