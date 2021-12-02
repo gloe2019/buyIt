6abc7452d7b54019ef0d5fdefca7f890a7219a80
gloe2019 <gloedem@gmail.com>
2021-12-01 23:25:15 -0500

    add order + shoppingCart models

---
+++ server/models/orderModel.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+interface Order {
+  userId: string;
+  items: { productId: string; name: string; quantity: number; price: number }[];
+  total: number;
+  dateAdded: Date;
+}
+
+const OrderSchema: Schema = new Schema<Order>({
+  userId: {
+    type: String,
+    required: true,
+  },
+  items: [
+    {
+      productId: {
+        type: String,
+        required: true,
+      },
+      name: { type: String },
+      quantity: {
+        type: Number,
+        min: [1],
+        required: true,
+      },
+      price: { type: Number },
+    },
+  ],
+  total: {
+    type: Number,
+    required: true,
+  },
+  dateAdded: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+export default mongoose.model<Order>("Order", OrderSchema);
