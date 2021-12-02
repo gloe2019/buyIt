960594218a9f5e90f7af622ba71b6074bda89cbd
gloe2019 <gloedem@gmail.com>
2021-12-02 13:16:53 -0500

    pass userController functions to appropriate routes

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,8 +1,11 @@
-import express from 'express'
-import controller from '../controllers/userController.js'
+import express from "express";
+import controller from "../controllers/userController.js";
+import extractJWT from "../middleware/extractJWT.js";
 
-const usersRouter = express.Router()
+const usersRouter = express.Router();
 
-usersRouter.get('/all', controller.getAllUsers)
-
-export {usersRouter}
+usersRouter.get("/validate", extractJWT, controller.validateToken);
+usersRouter.post("/register", controller.register);
+usersRouter.post("/login", controller.login);
+usersRouter.get("/all", controller.getAllUsers);
+export { usersRouter };
