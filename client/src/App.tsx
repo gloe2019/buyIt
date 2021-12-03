78c932d2d881b12bada21c2026f5dbb8b8ab64ef
gloe2019 <gloedem@gmail.com>
2021-12-02 21:42:25 -0500

    update App to pass values from useData hook to app routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,35 @@
-import { BrowserRouter, Route, } from "react-router-dom"
-import { Routes } from "react-router-dom"
-import routes from "./config/routes"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+import routes from "./config/routes";
+import useData from "./hooks/useData";
+
 // Routing should be done from here --> connect to backend!
 
-const App: React.FunctionComponent<{}> = props => {
+const App: React.FunctionComponent<{}> = (props) => {
+  const { register, login, user, token } = useData()
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
           {routes.map((route, index) => {
-            
             return (
               <Route
                 key={index}
                 path={route.path}
-                element={<route.component name={route.name}/>}
+                element={<route.component name={route.name}
+                  properties={{ register, login, user, token }}
+                />}
               />
-            )
+            );
           })}
           {/* <Route path="/" element={<Home name="Home"/>} />
           <Route path="/about" element={<About name="About"/>} /> */}
-      </Routes>
+        </Routes>
       </BrowserRouter>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
