5dab0c778f2ddb971adf321562dc74e773c0d3d0
gloe2019 <gloedem@gmail.com>
2021-12-04 12:11:08 -0500

    update routing + props being passed down

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,45 @@
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import routes from "./config/routes";
+// import routes from "./config/routes";
 import useData from "./hooks/useData";
-
+import Home from "./components/Home";
+// import About from "./components/About";
+import Login from "./components/Login";
+import Register from "./components/Register";
 // Routing should be done from here --> connect to backend!
 
-const App: React.FunctionComponent<{}> = (props) => {
-  const { register, login, user, token } = useData()
+const App = () => {
+  const { register, login, state, setState } = useData()
+  useEffect(() => {
+    let _user = localStorage.getItem('user')
+    let _token = localStorage.getItem('token')
+    if (_user && _token) {
+      setState({
+        ...state,
+        user: _user,
+        token: _token
+      })
+    }
 
+  }, [])
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          {routes.map((route, index) => {
+          {/* {routes.map((route, index) => {
             return (
               <Route
                 key={index}
                 path={route.path}
                 element={<route.component name={route.name}
-                  properties={{ register, login, user, token }}
+                  properties={{ register, login, state }}
                 />}
               />
             );
-          })}
-          {/* <Route path="/" element={<Home name="Home"/>} />
-          <Route path="/about" element={<About name="About"/>} /> */}
+          })} */}
+          <Route path="/" element={<Home name="Home" state={state} />} />
+          <Route path="/register" element={<Register name="Register" function={register} />} />
+          <Route path="/login" element={<Login name="Login" function={login} state={state} />} />
         </Routes>
       </BrowserRouter>
     </div>
