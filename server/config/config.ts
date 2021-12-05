f6ea42bd062bd0df579e9a4ecdc4bf11f358987b
gloe2019 <gloedem@gmail.com>
2021-12-05 09:06:07 -0500

    install stripe + update config w/ stripe keys

--- server/config/config.ts
+++ server/config/config.ts
@@ -7,6 +7,10 @@ const DB_URI = process.env.MONGODB_URI;
 const DB_NAME = process.env.DB_NAME || "BuyItDatabase";
 const SERVER_TOKEN_EXPIRES = process.env.TOKEN_EXPIRES || 1800;
 const SERVER_TOKEN_SECRET = process.env.TOKEN_SECRET || "verySecr3t!";
+const STRIPE_PUBLISHABLE_KEY =
+  process.env.STRIPE_PK || "get-your-own-stripe-pk";
+const STRIPE_SECRET_KEY =
+  process.env.STRIPE_SECRET || "get-your-own-stripe-secret";
 
 const SERVER = {
   hostname: SERVER_HOSTNAME,
@@ -16,6 +20,10 @@ const SERVER = {
   token: {
     expireTime: SERVER_TOKEN_EXPIRES,
     secret: SERVER_TOKEN_SECRET,
+  },
+  stripe: {
+    pubkey: STRIPE_PUBLISHABLE_KEY,
+    secret: STRIPE_SECRET_KEY,
   },
 };
 
